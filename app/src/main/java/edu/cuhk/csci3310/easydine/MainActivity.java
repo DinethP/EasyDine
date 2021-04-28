f1e6371b158877c81db272283b5c599eca3679c4
unknown <dineth_perera@hotmail.com>
2021-04-28 17:32:23 +0530

    Added Google Login and Dashboard

--- app/src/main/java/edu/cuhk/csci3310/easydine/MainActivity.java
+++ app/src/main/java/edu/cuhk/csci3310/easydine/MainActivity.java
@@ -3,12 +3,26 @@ package edu.cuhk.csci3310.easydine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GoogleSignInAccount account =getIntent().getParcelableExtra("ACCOUNT");
+        ImageView profilePic = findViewById(R.id.profilePic);
+        TextView title = findViewById(R.id.greeting);
+        title.setText("Hello " + account.getGivenName());
+//        profilePic.setImageURI(account.getPhotoUrl());
+
+        Log.d(TAG, "photo url: " + account.getPhotoUrl());
     }
 }
