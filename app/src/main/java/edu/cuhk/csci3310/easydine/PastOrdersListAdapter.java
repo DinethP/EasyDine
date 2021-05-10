e191341453b53953ef4050b0dee53b544091bcd4
Dineth Perera <dineth_perera@hotmail.com>
2021-05-10 08:26:51 +0530

    UI changes to PastOrdersActivity

--- app/src/main/java/edu/cuhk/csci3310/easydine/PastOrdersListAdapter.java
+++ app/src/main/java/edu/cuhk/csci3310/easydine/PastOrdersListAdapter.java
@@ -101,7 +101,8 @@ public class PastOrdersListAdapter extends RecyclerView.Adapter<PastOrdersListAd
         new DownloadImageTask(holder.restaurantImageView).execute(mImageURL);
         holder.nameTextView.setText(mRestaurantName);
         holder.dateTextView.setText(mDate);
-        holder.friendsTextView.setText(Integer.toString(mFriends));
+//        holder.friendsTextView.setText(R.string.friends_label + Integer.toString(mFriends));
+        holder.friendsTextView.setText(String.format("# of Friends:: %s", Integer.toString(mFriends)));
         if (mIsPayed) {
             holder.isPayedButton.setText("Already Paid");
             holder.isPayedButton.setEnabled(false);
